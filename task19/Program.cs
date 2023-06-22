22c6e0310e57911c77e38f74757fb33fbadd954a
Anton_Fursov <fursila02@mail.ru>
2023-06-23 00:04:20 +0300

    Homework3

---
+++ task19/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int a = N%10;
+int b = N%100/10;
+int c = N/10000;
+int d = N/1000%10;
+if (a == c) {
+    if (b==d) Console.WriteLine ("палиндром");
+} 
+else Console.WriteLine ("не палиндром");
---
+++ task23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите число");
+int N = int.Parse(Console.ReadLine ()!);
+int i = 1;
+while (i<=N){
+    Console.Write (i*i*i);
+    Console.Write (" ");
+    i++;
+}
+
